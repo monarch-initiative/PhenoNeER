a62e020c192e1ed5bd24d950e3d99b0cec2eba0c
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-11-18 16:16:53 -0500

    Draft `v0.0.1`.

--- phenoneer-model/src/main/java/org/monarchinitiative/phenoneer/model/package-info.java
+++ phenoneer-model/src/main/java/org/monarchinitiative/phenoneer/model/package-info.java
@@ -1,4 +1,4 @@
 /**
- * The package with data model and the default implementation of the BioNER API components.
+ * The package with data model and the default implementation of the PhenoNeER API components.
  */
 package org.monarchinitiative.phenoneer.model;
